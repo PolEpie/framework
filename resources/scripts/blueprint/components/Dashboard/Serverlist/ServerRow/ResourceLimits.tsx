e1113984ee6e4eee4ec9a1650d5233ee61520dc0
Pol Epié <36932762+PolEpie@users.noreply.github.com>
2025-12-11 19:42:21 +0100

    Oups

--- resources/scripts/blueprint/components/Dashboard/Serverlist/ServerRow/ResourceLimits.tsx
+++ resources/scripts/blueprint/components/Dashboard/Serverlist/ServerRow/ResourceLimits.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Server } from '@/api/server/getServer';
 /* blueprint/import */
 
-export default (arg: Server) => {
+export default ({arg: Server}) => {
   return (
     <>
       {/* blueprint/react */}
